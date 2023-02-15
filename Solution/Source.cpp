aa3f105a03705e1a7e547852dbcb8f488cefe6a3
Lukeduke91 <gurneylj@gmail.com>
2023-02-15 18:25:57 -0500

    Added Assignment 1

---
+++ Solution/Source.cpp
@@ -0,0 +1,14 @@
+#include <windows.h>
+
+#include <DirectXMath.h>
+
+#include <iostream>
+
+using namespace std;
+
+using namespace DirectX;
+
+int main()
+{
+	XMVECTOR ComputeNormal(FXMVECTOR p0, FXMVECTOR p1, FXMVECTOR p2);
+}
